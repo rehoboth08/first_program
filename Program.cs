7478d52b92d8eed0026ec0dbe769cccfb3cb8030
rehoboth08 <160334045+rehoboth08@users.noreply.github.com>
2024-02-19 09:35:38 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿class Program
+{
+
+
+    static void Main(string[] args)
+    {
+
+        //if statement = a basic form of decision making
+
+
+        Console.WriteLine("Please enter your age ");
+       // int age = Convert.ToInt32(Console.ReadLine());
+        string age_1 = Console.ReadLine();
+        if (age_1 == "emi")
+
+        {
+            Console.WriteLine("You are now signed up!"); 
+        }
+
+        Console.ReadKey();
+
+
+
+
+
+
+
+    }
+
+
+}
+
+
+
+
